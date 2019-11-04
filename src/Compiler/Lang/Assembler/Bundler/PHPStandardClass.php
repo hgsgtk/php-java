a55052259a25eef94766c3a6e3efe271da6b2e2c
memory-agape <memory.style.serv@gmail.com>
2019-11-04 16:14:09 +0900

    Update

--- src/Compiler/Lang/Assembler/Bundler/PHPStandardClass.php
+++ src/Compiler/Lang/Assembler/Bundler/PHPStandardClass.php
@@ -73,6 +73,7 @@ class PHPStandardClass extends AbstractBundler
                         (new Field(
                             (new FieldAccessFlag())
                                 ->enablePublic()
+                                ->enableStatic()
                                 ->enableFinal()
                                 ->make(),
                             $this->getEnhancedConstantPool()
@@ -93,60 +94,6 @@ class PHPStandardClass extends AbstractBundler
                     )
                 );
             }
-
-//            foreach ($packageInstance->getDefinedMethods() as [$methodName, $argumentSignature, $returnSignature]) {
-//                $defaultMethodName = $methodName;
-//                $methodName = Runtime::PHP_STANDARD_CLASS_METHOD_PREFIX . $methodName;
-//
-//                $descriptor = new Descriptor();
-//                foreach ($argumentSignature as $argument) {
-//                    $descriptor
-//                        ->addArgument($argument);
-//                }
-//                $descriptor = $descriptor
-//                    ->setReturn($returnSignature)
-//                    ->make();
-//
-//                $this->getEnhancedConstantPool()
-//                    ->addMethodref(
-//                        $className,
-//                        $methodName,
-//                        $descriptor
-//                    );
-//
-//                // Call functions
-//                $functionOperationCodes = call_user_func([$packageInstance, $defaultMethodName]);
-//
-//                $this->methods
-//                    ->add(
-//                        (new Method(
-//                            (new \PHPJava\Compiler\Builder\Signatures\MethodAccessFlag())
-//                                ->enablePublic()
-//                                ->enableStatic()
-//                                ->make(),
-//                            $this->getConstantPoolFinder()
-//                                ->find(
-//                                    Utf8Info::class,
-//                                    $methodName
-//                                ),
-//                            $this->getConstantPoolFinder()->find(
-//                                Utf8Info::class,
-//                                $descriptor
-//                            )
-//                        ))
-//                            ->setAttributes(
-//                                (new Attributes())
-//                                    ->add(
-//                                        (new Code())
-//                                            ->setConstantPool($this->getConstantPool())
-//                                            ->setConstantPoolFinder($this->getConstantPoolFinder())
-//                                            ->setCode($functionOperationCodes)
-//                                            ->beginPrepare()
-//                                    )
-//                                    ->toArray()
-//                            )
-//                    );
-//            }
         }
 
         $this->getEnhancedConstantPool()
